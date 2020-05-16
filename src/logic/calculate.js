4e7f22ab520b7197ffe61f41e2f34b55b4542e3b
David <adejo.david@aiesec.net>
2020-05-17 00:49:58 +0100

    to iterate through based on operator for calculations

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,6 +1,33 @@
 function calculate(arg) {
     const values = arg.match(/(\d+\.\d+|\d+)|\D/g)
+    const operators = ["*", "/", "+", "-"]
 
+    const result = values.reduce((acc, val, ind, arr) => {
+        let back;
+        if (ind > 0) {
+
+            acc = arr[ind - 1] + acc;
+            back = arr[ind + 1];
+
+        }
+        console.log(acc)
+        console.log(back)
+        switch (val) {
+            case "+":
+                return acc = (acc + back);
+            case "-":
+                return acc = (acc - back);
+            case "/":
+                return acc = (acc / back);
+            case "*":
+                return acc = (acc * back);
+            default:
+                return acc;
+        }
+
+    }, 0)
+
+    console.log(result)
     return values
 }
 
